5db04982773c4b96fc2ed09e7786ca27eb34e24a
Jin Lou <jinlou@bu.edu>
2021-05-23 09:41:30 -0400

    Manage error states.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age",
+      });
       return;
     }
 
@@ -31,13 +40,20 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
       {" "}
-      <ErrorModal
-        title="An error occurred"
-        message="Something went wrong!"
-      ></ErrorModal>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></ErrorModal>
+      )}
       {/* // the function to call when the form is submitted
     // no parenthesis!!! we just want to pass a pointer */}
       <Card className={classes.input}>
